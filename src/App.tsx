4ab7f072c83bd0a88ea2ec3914176d82d7a3b3ec
Mladjan Milosavljevic <mladjan@quantlabs.rs>
2023-02-28 21:12:39 +0100

    style: Add linters

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className='App'>
+      <header className='App-header'></header>
     </div>
   );
 }
